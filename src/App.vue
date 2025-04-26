f5b7bce4f74c04ded60ab966b12c130530800a33
Seri0usBlack <seriousblack.pro@gmail.com>
2025-04-26 17:51:53 +0200

    Mise en place du slider et des différents composants, en intégrant du GSAP pour les transitions. Modification ulterieures pour la durée des slides, et également rattaché la sidebar au slides

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
-  <Navigation />
   <Sliders />
 
 </template>
 
 <script setup lang="ts">
-import Navigation from './components/Navigation.vue';
 import Sliders from './components/Sliders.vue';
 
 </script>
--- src/components/Sliders.vue
+++ src/components/Sliders.vue
@@ -1,25 +1,59 @@
 <template>
-    <About />
-    <Projects />
-    <TechStack />
-    <Contact />
+    <div class="slider-container">
+        <Navigation :active-index="activeIndex" :titles="slidesTitles" />
+    
 
+        <div class="slides-viewport">
+            <div class="slides-wrapper" ref="slidesWrapper">
+                <component v-for="(SlideComponent, index) in slides"
+                :is="SlideComponent" :key="index" class="slide" />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
+import {ref, onMounted } from 'vue';
+import { gsap } from 'gsap';
 import About from './About.vue';
 import Projects from "./Projects.vue";
 import TechStack from "./TechStack.vue";
 import Contact from "./Contact.vue";
+import Navigation from "./Navigation.vue"
 
 export default {
-    name:"Sliders",
     components: {
+        Navigation,
         About,
         Projects,
         TechStack,
-        Contact,
+        Contact
+    },
+    setup() {
+        const activeIndex = ref(0);
+        const slidesWrapper = ref(null);
 
+        const slides = [About, Projects, TechStack, Contact];
+        const slidesTitles = ['About', 'Projects', 'Tech Stack', "Contact"];
+
+        const animateSlide = (index) =>{
+            gsap.to(slidesWrapper.value, {
+                y: -index * window.innerHeight,
+                duration: 1,
+                ease: "power2.inOut"
+            });
+        };
+
+        onMounted(() => {
+            setInterval(() => {
+                const nextIndex = (activeIndex.value + 1) % slides.length;
+                activeIndex.value = nextIndex;
+                animateSlide(nextIndex);
+            }, 4000);
+        });
+
+        return { activeIndex, slidesWrapper, slides, slidesTitles};
+    
     }
 }
 
@@ -28,4 +62,24 @@ export default {
 
 <style scoped>
 
+.sliders-container {
+  display: flex;
+}
+
+.slides-viewport {
+  overflow: hidden;
+  height: 100vh;
+  width: 100%;
+  position: relative;
+}
+
+.slides-wrapper {
+  display: flex;
+  flex-direction: column;
+}
+
+.slide {
+  height: 100vh;
+  width: 100%;
+}
 </style>
